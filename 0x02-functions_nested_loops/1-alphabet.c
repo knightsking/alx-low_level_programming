1844e4a701eed8963b1a6c71c8c0955990004eb0
Samuel <ajatasamuel2051@gmail.com>
2022-12-14 13:19:24 -0800

    aplha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alpha in lowercase
+ * print_alphabet- prints the alphabet, in lowercase
+ * followed by a new line
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
-	char lc;
+	char alphabet = 'a';
 
-	for (lc = 'a'; lc <= 'z'; lc++)
-		_putchar(lc);
-
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
 	_putchar('\n');
 }
