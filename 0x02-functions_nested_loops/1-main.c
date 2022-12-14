6af1e981545da96fd54461f20b411164e0efb7cb
Samuel <ajatasamuel2051@gmail.com>
2022-12-14 13:49:44 -0800

    print

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -4,10 +4,8 @@
 *
 *return: always return 0
 */
-
 int main(void)
 {
 	print_alphabet();
-
 	return (0);
 }
--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -4,4 +4,8 @@
 *
 *Return: Always 0
 */
-
+int main(void)
+	{
+	    print_alphabet_x10();
+	    return (0);
+	}
